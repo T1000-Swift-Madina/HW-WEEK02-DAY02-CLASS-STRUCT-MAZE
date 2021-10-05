de664ff47368d4180d5c4ab937ee98cde2e4ccac
nada_alansari <nada0an90@gmail.com>
2021-10-05 19:17:01 +0300

    fixed some issues in code

--- Maze.playground/Contents.swift
+++ Maze.playground/Contents.swift
@@ -2,11 +2,11 @@ import UIKit
 
 
 class maze {
-     var arr = [[String]]()
-     var isVisited: Bool
-     var isWall: Bool
-     var currentPossitionX: Int
-     var currentPossitionY: Int
+        var arr = [[String]]()
+    var isVisited: Bool
+    var isWall: Bool
+    var currentPossitionX: Int
+    var currentPossitionY: Int
     
     init(arr: [[String]], isVisited : Bool, isWall: Bool ,  currentPossitionX :Int, currentPossitionY :Int){
         
@@ -17,101 +17,84 @@ class maze {
         self.currentPossitionY = currentPossitionY
         
     }
-
-
+    
+    
     func goRight(currentPossition1 :Int , currentPossition2 :Int ){
         let x = currentPossition1
         let y = currentPossition2
-        if(x > 1 || y > 1){
-            print("out of range")
-
-        }
-        else if (x > 1 || y >= 1){
+   
+         if ( y == 1){
             isWall = true
-            print("wall")
+            print("there is a wall 🚪")
         }
         else if(arr[x][y+1] == "✅"){
-              isVisited = true
-              print("is visited alrady ")
-                }
-   
-        else if(x > 1){
-        isWall = true
-        print("there is a wall 🚪 ")
-
+            isVisited = true
+            print("is visited alrady ")
         }
-        else if(arr[x][y] != "✅" && y < 1){
+        
+        else if(arr[x][y+1] != "✅" ){
             arr[x][y+1] = "✅"
-            print("Now it will ✅ ")
             print(arr)
-
+            
         }
-                
+        
         else{
             print("out of range ")
-
+            
         }
-
+        
     }
     
     func goLeft(currentPossition1 :Int , currentPossition2 :Int ){
-        
         let x = currentPossition1
         let y = currentPossition2
-        if(x > 1 || y > 1){
-            print("out of range")
-
-        }
-        else if (y != 1){
+        if (y ==  0 ){
             isWall = true
-            print("wall")
+            print("there is a wall 🚪")
         }
         else if(arr[x][y-1] == "✅"){
             isVisited = true
             print("is visited alrady ")
         }
-  
         
         else if(arr[x][y-1] != "✅"){
             arr[x][y-1] = "✅"
-            print("Now it will  ✅ ")
             print(arr)
-
+            
         }
         else{
             print("out of range ")
-
+            
         }
         
     }
-
+    
     func goTop(currentPossition1 :Int , currentPossition2 :Int ){
+        //1,0
         let x = currentPossition1
         let y = currentPossition2
         
         if(x > 1 || y > 1 ){
             print("out of range")
-
+            
         }
-         else if(x == 0 ){
+        else if(x == 0 ){
             isWall = true
             print("there is a wall 🚪 ")
-
+            
         }
         else if(arr[x-1][y] == "✅"){
-           isVisited = true
+            isVisited = true
             print("is visited alrady ")
         }
         
         else if(arr[x-1][y] != "✅"){
             arr[x-1][y] = "✅"
-            print("Now it will ✅ ")
             print(arr)
-
         }
         else{
             print("out of range ")
-
+            
         }
         
     }
@@ -120,27 +103,27 @@ class maze {
         let y = currentPossition2
         if(x > 1 || y > 1 ){
             print("out of range")
-
+            
         }
-         else if(x == 1 ){
+        else if(x > 1 ){
             isWall = true
             print("there is a wall 🚪 ")
-
+            
         }
         else if(arr[x+1][y] == "✅"){
             isVisited = true
             print("is visited alrady ")
         }
-   
+        
         else if(arr[x+1][y] != "✅"){
             arr[x+1][y] = "✅"
             print("Now it will ✅ ")
             print(arr)
-
+            
         }
         else{
             print("out of range ")
-
+            
         }
         
     }
@@ -149,62 +132,40 @@ class maze {
         var validate = true
         for subarray in arr {
             for element in subarray {
-                if(element != "✅"){
+                if(element == "❌"){
                     validate = false
                     break
                 }
-
+                
                 
             }
         }
         print(validate)
-
+        
     }
-
+    
     
     
     
 }
 var m = maze(arr: [["❌","❌"],["❌","❌"]], isVisited: false, isWall: false, currentPossitionX: 0, currentPossitionY: 0)
 print("Array before visited")
-
 print(m.arr)
 
+//test moving
+m.goBottom(currentPossition1: 0, currentPossition2: 0)
+m.goRight(currentPossition1: 1, currentPossition2: 0)
+m.goLeft(currentPossition1: 1, currentPossition2: 1)
+m.goTop(currentPossition1: 1, currentPossition2: 0)
+m.goRight(currentPossition1: 0, currentPossition2: 0)
+m.goRight(currentPossition1: 0, currentPossition2: 0)
 
 
-print("Going right")
-
-print(m.goRight(currentPossition1: 0, currentPossition2: 0))
-print(m.goRight(currentPossition1: 0, currentPossition2: 1))
-print(m.goRight(currentPossition1: 1, currentPossition2: 0))
-print(m.goRight(currentPossition1: 1, currentPossition2: 1))
-
-print("Going left")
-
-print(m.goLeft(currentPossition1: 0, currentPossition2: 0))
-print(m.goLeft(currentPossition1: 0, currentPossition2: 1))
-print(m.goLeft(currentPossition1: 1, currentPossition2: 0))
-print(m.goLeft(currentPossition1: 1, currentPossition2: 1))
-
-
-print("Going top")
-
-print(m.goTop(currentPossition1: 0, currentPossition2: 0))
-print(m.goTop(currentPossition1: 0, currentPossition2: 1))
-print(m.goTop(currentPossition1: 1, currentPossition2: 0))
-print(m.goTop(currentPossition1: 1, currentPossition2: 1))
-
-print("Going down")
-
-print(m.goBottom(currentPossition1: 0, currentPossition2: 0))
-print(m.goBottom(currentPossition1: 0, currentPossition2: 1))
-print(m.goBottom(currentPossition1: 1, currentPossition2: 0))
-print(m.goBottom(currentPossition1: 1, currentPossition2: 1))
-
-
-
-
-
+//test the wall
+m.goTop(currentPossition1: 0, currentPossition2: 1)
+m.goBottom(currentPossition1: 2 , currentPossition2:0 )
+m.goRight(currentPossition1: 0 , currentPossition2:1 )
+m.goLeft(currentPossition1: 1, currentPossition2: 0)
 
 print("Array after visited")
 print(m.arr)
