aa83d56bc0c4c34f5e2bbc4bc51ea72d053ad282
Abdullah Bajaman <ambajaman@gmail.com>
2021-10-05 23:35:18 +0300

    edit: delete checkMove function, edit dirictions and add isComplate function

--- StrictsAndClasses.playground/Contents.swift
+++ StrictsAndClasses.playground/Contents.swift
@@ -9,9 +9,9 @@ import UIKit
 
 /* scenario
 1. check if user started
- 2. start in top right
+ 2. start in top right or bottom right
  3. make a first move
- 4. check if could preform next move or there is wall
+ 4. check if could perform next move or there is wall
  5. if wall, stay same position, print message, show options to move to another direction
  6. if go through all space End
  */
@@ -22,7 +22,6 @@ class Maze {
     var trackPosition = [Int]()
     var name: String, message: String, input: String
     init() {
-        
         self.currentPosition = 0
         self.isWall = false
         self.isStart = false
@@ -31,17 +30,13 @@ class Maze {
         self.message = "Wilcome to The Maze"
         self.input = ""
         print(message)
-        
     }
-//    init(currentPosition : Int  = 0 , isWall: Bool = false, isStart: Bool=false, isVisited: Bool=false) {
-//        self.currentPosition = currentPosition
-//        self.isWall = isWall
-//        self.isStart = isStart
-//        self.isVisited = isVisited
-//    }
+
     func displayMaze () {
+        print("    ______")
         print("➡️ |\(boardArr[0])|\(boardArr[1])|")
         print("   |\(boardArr[2])|\(boardArr[3])|")
+        print("    ______")
     }
     func startMaze () {
         setIsStart(isStart: true)
@@ -50,11 +45,9 @@ class Maze {
     func setIsStart(isStart:Bool){
         self.isStart = isStart
     }
-    func getIsStart()->Bool{
-        return self.isStart
-    }
+    
     func move(direction: String) {
-        if checkMove() {
+//        if checkMove() {
             
             print("go to \(direction)")
             switch direction {
@@ -69,28 +62,36 @@ class Maze {
             default:
                 "Please Type one of this dirctions: right, left, top, bottom"
             }
-        }
+//        }
     }
-
-    func checkMove () -> Bool{
-        // check is started , visited, wall,
-        
-        let check = isStart && !isVisited[currentPosition] ? true :  false
-        return check
-    }
+// check next visit
+    //toggle wall 
+//    func checkMove () -> Bool{
+//        // check is started , visited, wall,
+//        if !isStart { print("Please Start the Mase")}
+//        let check = !isVisited[currentPosition] ? true :  false
+//        return check
+//    }
     
     func goRight() {
         if !boardArr[0].contains("👤") && !boardArr[1].contains("👤") && !boardArr[2].contains("👤") && !boardArr[3].contains("👤") {
-            boardArr[0] = "👤"
-            displayMaze()
+            if isVisited[0] == false {
+                boardArr[0] = "👤"
+                isVisited[0] = true
+                print(currentPosition)
+                displayMaze()
+            }
         }
         else if currentPosition == 0 || currentPosition == 2{
             boardArr[currentPosition] = "  "
-            isVisited[currentPosition] = true
             currentPosition += 1
-            boardArr[currentPosition] = "👤"
-            displayMaze()
-            isComplete()
+            print(currentPosition)
+            if !isVisited[currentPosition] {
+                boardArr[currentPosition] = "👤"
+                isVisited[currentPosition] = true
+                displayMaze()
+                isComplete()
+            }
         } else {
             displayMaze()
             print("there is no Exit here")
@@ -100,11 +101,13 @@ class Maze {
         print("left")
         if currentPosition == 1 || currentPosition == 3{
             boardArr[currentPosition] = "  "
-            isVisited[currentPosition] = true
             currentPosition -= 1
-            boardArr[currentPosition] = "👤"
-            displayMaze()
-            isComplete()
+            if !isVisited[currentPosition] {
+                boardArr[currentPosition] = "👤"
+                isVisited[currentPosition] = true
+                displayMaze()
+                isComplete()
+            }
         } else {
             displayMaze()
             print("there is no Exit here")
@@ -114,11 +117,13 @@ class Maze {
         print("bottom")
         if currentPosition == 0 || currentPosition == 1{
             boardArr[currentPosition] = "  "
-            isVisited[currentPosition] = true
             currentPosition += 2
-            boardArr[currentPosition] = "👤"
-            displayMaze()
-            isComplete()
+            if !isVisited[currentPosition] {
+                boardArr[currentPosition] = "👤"
+                isVisited[currentPosition] = true
+                displayMaze()
+                isComplete()
+            }
         } else {
             displayMaze()
             print("there is no Exit here")
@@ -126,23 +131,25 @@ class Maze {
     }
     func goTop () {
         print("top")
-        if currentPosition == 3 {
+        if currentPosition == 3 || currentPosition == 2 {
             boardArr[currentPosition] = "  "
-            isVisited[currentPosition] = true
             currentPosition -= 2
-            boardArr[currentPosition] = "👤"
-            displayMaze()
-            isComplete()
+            if !isVisited[currentPosition] {
+                boardArr[currentPosition] = "👤"
+                isVisited[currentPosition] = true
+                displayMaze()
+                isComplete()
+            }
         } else {
             displayMaze()
             print("there is no Exit here")
         }
     }
     func isComplete () {
-        if isVisited.contains(false) {
+        if !isVisited.contains(false) {
             print("Congratulation You Complated the Maze ")
         }else {
-            print("try again")
+            print("Keep trying ")
         }
     }
     
@@ -156,23 +163,18 @@ player1
 // start maze
 player1.startMaze()
 
-//var boardArr = [
-//["👤", " "],
-//[" ", " "]
-//]
-//boardArr[0][0]
-// choose a direction
 
-player1.move(direction: "left")
+player1.move(direction: "r")
 sleep(1)
-
-player1.move(direction: "right")
-sleep(1)
+player1.currentPosition
 
 player1.move(direction: "bottom")
 sleep(1)
 
-player1.move(direction: "left")
+player1.move(direction: "r")
+sleep(1)
+
+player1.move(direction: "top")
 sleep(1)
 
 player1.move(direction: "top")
